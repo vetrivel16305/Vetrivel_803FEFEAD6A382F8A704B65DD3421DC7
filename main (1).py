f6589c9987fb4a1bbde76bd0a4bf2e5833a7ab96
vetrivel16305 <144667607+vetrivel16305@users.noreply.github.com>
2023-09-11 19:32:46 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+def checkleap(year):
+  if((year%400==0)or(year%100!=0)and(year%4==0)):
+    print("given year is a leap year")
+  else:
+    print("given year is not a leap year")
+year=int(input("enter the year:"))
+checkleap(year)
